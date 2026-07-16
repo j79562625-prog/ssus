18ebef4f0df293907b0710e55e8a6e9414d1a1a8
j79562625-prog <j79562625@gmail.com>
2026-07-16 16:57:21 +0530

    Update mongodb.py

--- database/mongodb.py
+++ database/mongodb.py
@@ -8,7 +8,7 @@ try:
 
     client.admin.command("ping")
 
-    db = client["ssus123"]
+    db = client["ssus1234"]
      
     students_collection = db["students"]
     marks_collection = db["marks"]
